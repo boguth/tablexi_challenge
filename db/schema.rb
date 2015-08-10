892a3230dafae0bcb00c55aa43be8232b4196d77
boguth <bo.guthrie.mail@gmail.com>
2015-08-10 18:32:17 -0500

    Got postgres db set up for heroku deployment.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150810161832) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "text_files", force: :cascade do |t|
     t.float    "target"
     t.string   "lines"
